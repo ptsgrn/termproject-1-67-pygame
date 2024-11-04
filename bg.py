34a6a45cf5c2a8bbeed3bcc8e8ec22515da02bbc
Papitchya Bunchuchai <papitchyabunchuchai@Papitchya.local>
2024-11-04 15:31:31 +0700

    ทำ Display Full Screen รอเอาปุ่มกับ BG เข้ามาใส่

--- bg.py
+++ bg.py
@@ -1,3 +1,19 @@
-import pygame 
+import pygame
+
 pygame.init()
+
+pygame.display.set_caption("Chula life")
+screen = pygame.display.set_mode((800,600), pygame.FULLSCREEN)
+clear_color = (30, 150, 50)
+running = True
+
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #draw code
+    screen.fill(clear_color)
+
+    pygame.display.flip 
+pygame.quit()
 
