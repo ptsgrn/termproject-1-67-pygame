f9e7939e053e640c1dba827f911eb1d666b630b8
Papitchya Bunchuchai <papitchyabunchuchai@Papitchya.local>
2024-11-10 18:10:40 +0700

    ทำตัวละครหลัก กำลังเตรียมใส่ Png แล้วก็ปุ่มเดิน

---
+++ chulalife/mainCharacter.py
@@ -0,0 +1,16 @@
+import pygame 
+
+pygame init()
+player_image = pygame.image.load('')
+player
+
+ 
+def move_player(keys):
+    if keys[pygame.K_LEFT]:
+        player_pos[0] -= 5
+    if keys[pygame.K_RIGHT]:
+        player_pos[0] += 5
+    if keys[pygame.K_UP]:
+        player_pos[1] -= 5
+    if keys[pygame.K_DOWN]:
+        player_pos[1] += 5
