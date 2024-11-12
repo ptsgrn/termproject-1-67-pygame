edd5a7342059f7aa51d92a2605682b390157434e
Papitchya Bunchuchai <papitchyabunchuchai@Papitchya.local>
2024-11-13 00:55:46 +0700

    ทำด่านใหม่ กับ ลบBg

---
+++ chulalife/Q1/images/game.py
@@ -0,0 +1,21 @@
+import pygame 
+
+pygame.init()
+
+pygame.display.set_caption("Chula life")
+screen = pygame.display.set_mode((800,600))
+clear_color = (30, 150, 50)
+running = True
+
+#Game Loop 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    #draw code 
+    screen.fill(clear_color)
+
+    pygame.display.flip()
+
+pygame.quit()
