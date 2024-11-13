5c3087a9392cb27aef5bd3b9c77b5526ac4358f4
Papitchya Bunchuchai <papitchyabunchuchai@Papitchya.local>
2024-11-14 01:37:46 +0700

    load map file

--- chulalife/Q1/map.py
+++ chulalife/Q1/map.py
@@ -6,3 +6,20 @@ class Tilekind:
         self.image = pygame.image.load(image)
         self.is_solid = is_solid
 
+class Map:
+    def __init__(self, map_file, tile_kinds, tile_size):
+        self.tile_kinds = tile_kinds
+        
+    #load map file 
+    file = open(map_fiel, 'r')
+    data = file.read()
+    file.close()
+
+    #set the tiles from load data 
+    self.tiles = []
+
+        
+
+    #set tile loaded data 
+
+
