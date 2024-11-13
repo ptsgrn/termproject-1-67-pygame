5ef79d2143994ab70eb980b52a046222a5df21d2
Papitchya Bunchuchai <papitchyabunchuchai@Papitchya.local>
2024-11-13 17:26:49 +0700

    เพิ่มMap

--- chulalife/Q1/map.py
+++ chulalife/Q1/map.py
@@ -1,1 +1,8 @@
 import pygame 
+
+class Tilekind:
+    def __init__(self, name, image, is_solid):
+        self.name = name 
+        self.image = pygame.image.load(image)
+        self.is_solid = is_solid
+
