86c3967f4151f4e80006fa538881496037a913b8
Papitchya Bunchuchai <papitchyabunchuchai@Papitchya.local>
2024-11-13 16:04:48 +0700

    เปลี่ยนที่อยู่ไฟล์

--- chulalife/Q1/game.py
+++ chulalife/Q1/game.py
@@ -1,5 +1,7 @@
 import pygame 
 import input 
+from player import Player 
+from sprite import sprites 
 
 pygame.init()
 
@@ -7,6 +9,7 @@ pygame.display.set_caption("Chula life")
 screen = pygame.display.set_mode((800,600))
 clear_color = (30, 150, 50)
 running = True
+player = Player('chulalife/Q1/images/player.png', 0,0)
 
 #Game Loop 
 while running:
@@ -18,10 +21,12 @@ while running:
         elif event.type == pyagme.KEYUP:
             input.key_down.remove(event.key)
             
-    
+    #update code 
+    player.update()
     #draw code 
     screen.fill(clear_color)
-
+    for s in sprites:
+        s.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
