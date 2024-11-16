4b67eabdce6727af02e805a691c01264191c0094
Patsagorn <patsagorn_y@outlook.com>
2024-11-17 05:11:24 +0700

    ปรับปรุงคลาส StaticBackground เพื่อโหลดภาพพื้นหลังและเพิ่มฟังก์ชัน draw สำหรับการแสดงผล

--- chulalife/background.py
+++ chulalife/background.py
@@ -4,11 +4,12 @@ from pygame.typing import FileLike
 from pygame.surface import Surface
 from pygame.image import load
 
-from .helper import scale_fit
-
 
 class StaticBackground:
-    def __init__(self, image_file_name: FileLike, screen: Surface, *groups):
-        self.bg_image, self.bg_rect = scale_fit(
-            load(image_file_name), screen.get_rect())
-        screen.blit(self.bg_image, self.bg_rect)
+    def __init__(self, image_file_name: FileLike, *groups):
+        self.bg_image = load(image_file_name)
+
+    def draw(self, screen: Surface, offset: List[int] = [0, 0]):
+        self.bg_image = pygame.transform.scale(
+            self.bg_image, (screen.get_width(), screen.get_height()))
+        screen.blit(self.bg_image, offset)
